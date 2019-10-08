6de2e9c11faf4ca78065138f3641535ff4be3fb1
Deon Kloppers <gjkloppers@gmail.com>
2019-10-08 15:59:41 +0200

    Amper daar

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"log"
 	"math/rand"
 	"net/http"
+	"os"
 	"strconv"
 	"text/template"
 
@@ -115,5 +116,5 @@ func main() {
 	r.HandleFunc("/books/{id}", deleteBook).Methods("DELETE")
 
 	// Start server
-	log.Fatal(http.ListenAndServe(":3000", r))
+	log.Fatal(http.ListenAndServe(":"+os.Getenv("PORT"), r))
 }
